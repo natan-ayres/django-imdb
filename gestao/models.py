90e87861815715cb5e2cc924fccccd74b027430c
Natan Ayres <natan.nascimento.ayres@gmail.com>
2024-07-16 23:43:09 -0300

    Criado o modelo de Filmes e partial head

--- gestao/models.py
+++ gestao/models.py
@@ -1,4 +1,28 @@
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 from django.db import models
+from django.db.models import Avg
 from django.contrib.auth.models import User
 
-# Create your models here.
+class Filmes(models.Model):
+    nome = models.CharField(max_length=30)
+    desc = models.TextField(max_length=100)
+    data = models.DateField()
+    foto = models.ImageField(blank=True, upload_to= 'posters/')
+    nota_media = models.DecimalField(blank=True, null=True, max_digits=3, decimal_places=1)
+    avaliacoes = models.ManyToManyField(User, through='Reviews')
+
+    def calcular_nota_media(self):
+        reviews = self.reviews.all()
+        if reviews.exists():
+            media = reviews.aggregate(Avg('nota'))['nota__avg']
+            self.nota_media = media
+        else:
+            self.nota_media = None
+
+    def save(self, *args, **kwargs):
+        self.calcular_nota_media()
+        super(Filmes, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.nome}'
